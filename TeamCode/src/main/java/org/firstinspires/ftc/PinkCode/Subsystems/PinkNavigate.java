ed0c28713011b4fb629ff75e3c954fb8ed7ae28f
DevSamuelV <realalloutmusic@gmail.com>
2020-09-17 19:52:56 -0400

    Added Base PinkNavigate

--- TeamCode/src/main/java/org/firstinspires/ftc/PinkCode/Subsystems/PinkNavigate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/PinkCode/Subsystems/PinkNavigate.java
@@ -1,14 +1,31 @@
 package org.firstinspires.ftc.PinkCode.Subsystems;
 
-public class PinkNavigate {
-    public PinkNavigate() {
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.localization.ThreeTrackingWheelLocalizer;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class PinkNavigate extends ThreeTrackingWheelLocalizer {
+    public PinkNavigate() {
+        super(Arrays.asList(
+                new Pose2d(0.0,0.0),
+                new Pose2d(0.0,0.0),
+                // Math.toDegrees(90) for perpendicular
+                new Pose2d(0.0, 0.0, Math.toDegrees(90))
+        ));
     }
 
     public void DriveToPos(double leftF, double leftB, double rightF, double rightB) {
-//        Subsystem.robot.leftB_drive.setPower();
 
     }
 
+    @NotNull
+    @Override
+    public List<Double> getWheelPositions() {
+        return null;
+    }
 }
 
