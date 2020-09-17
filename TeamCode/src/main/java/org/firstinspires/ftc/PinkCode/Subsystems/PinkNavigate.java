33e700dfd6852859c9c9d372c56e6edd3a78b554
DevSamuelV <realalloutmusic@gmail.com>
2020-09-17 19:43:22 -0400

    Added RoadRunner To TeamCode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/PinkCode/Subsystems/PinkNavigate.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.PinkCode.Subsystems;
+
+public class PinkNavigate {
+    public PinkNavigate() {
+
+    }
+
+    public void DriveToPos(double leftF, double leftB, double rightF, double rightB) {
+//        Subsystem.robot.leftB_drive.setPower();
+
+    }
+
+}
+
